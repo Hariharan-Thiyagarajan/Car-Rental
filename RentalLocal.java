6eade36209c396c7b6d0fee2bacb63eafd490fe1
Hariharan <96095399+Hariharan-Thiyagarajan@users.noreply.github.com>
2022-01-25 14:13:41 +0530

    Update RentalLocal.java

--- RentalLocal.java
+++ RentalLocal.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class RentalLocal {
 	public static void main(String args[]) throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\M1077553\\Documents\\chrome\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Admin\\Documents\\chrome\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.savaari.com/");
 		driver.findElement(By.xpath("//*[@id=\"approot\"]/mat-sidenav-container/mat-sidenav-content/app-home/div/app-home-container/div[1]/div[1]/div[1]/div[2]/a/span")).click();
